7f5bd260badc5f34e65b74ba7d4959182a61db4b
Oksana Kalinicheva <fallen-post@mail.ru>
2020-08-21 01:39:36 +0300

    Revert "Update main.js"
    This reverts commit efb294e3300f3d90a9777280c88fb1690e70f7d2.

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
 		headerHeight = getHeaderHeight();
 	}	
 	$(window).resize(function () {
-		alert('Изменен размер окна');
 		if ($('.header').not('.fixed')) {
 			$('.header').removeClass('fixed');
 			headerHeight = getHeaderHeight();
